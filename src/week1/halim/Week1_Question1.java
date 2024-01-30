ac917fee6f3bea9a81a1a15ba7108787869b3451
hiltas73 <h_iltas@yahoo.com>
2024-01-31 00:20:00 +0300

    Halim Week1_Question1.java

---
+++ src/week1/halim/Week1_Question1.java
@@ -0,0 +1,17 @@
+package week1.halim;
+
+public class Week1_Question1 {
+    /*
+    Write  a method which can identifies given number is even or odd
+     */
+
+    public static void main(String[] args) {
+        System.out.println(identifyOddEven(5));
+
+    }
+    public static String identifyOddEven(int n) {
+        return n%2==0 ? "Even" : "odd" ;
+    }
+
+
+}
