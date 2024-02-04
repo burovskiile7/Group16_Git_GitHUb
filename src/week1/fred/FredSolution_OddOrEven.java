17263b37881b04c43f4f669cfc3ea4ad0adced0a
Fred <Frabin2023@gmail.com>
2024-02-04 13:05:48 -0800

    Updated FredSolution_OddOrEven

--- src/week1/fred/FredSolution_OddOrEven.java
+++ src/week1/fred/FredSolution_OddOrEven.java
@@ -10,3 +10,12 @@ public class FredSolution_OddOrEven {
         return String.format("%s", (number % 2 == 0) ? "Even" : "Odd");
     }
 }
+//String.format("%s", ...) : This method is used to format strings. In this case, it's formatting a string with a single placeholder (%s). The %s is a format specifier for a string. The value inside the parentheses after the comma is the argument that will be substituted into the placeholder.
+//
+//Putting it all together:
+//
+//If number is even, the ternary operator returns "Even".
+//If number is odd, the ternary operator returns "Odd".
+//The result is then passed to String.format, which formats it as a string using the %s placeholder. The entire expression is then returned as the result of the identify method.
+//
+//So, this method takes an integer as input, determines whether it's even or odd, and returns the result as a string ("Even" or "Odd").
