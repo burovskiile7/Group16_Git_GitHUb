ab49e7a57b49df1c9818de74334231624267d730
Nour <nourd64@hotmail.com>
2024-02-07 03:26:32 -0800

    solution task number2

--- src/week1/dusan/Question1.java
+++ src/week1/dusan/Question1.java
@@ -2,14 +2,12 @@ package week1.dusan;
 
 public class Question1 {
 
- Fred_Week1_Q1
 
 
     // add more questions
 
 // Tell me about yourself
 
- master
     //questions
     //new thing
     //Fred comment
