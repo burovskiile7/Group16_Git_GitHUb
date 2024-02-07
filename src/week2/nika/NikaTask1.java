61d1251b2b0daa48aa7acc06ca9127da783da087
Nika <nikapozdniakova@gmail.com>
2024-02-06 19:59:52 -0800

    task 1, week 2

---
+++ src/week2/nika/NikaTask1.java
@@ -0,0 +1,16 @@
+package week2.nika;
+
+public class NikaTask1 {
+    public static void main(String[] args) {
+        int x = 1, y = 2;
+
+        x = x + y; // 3
+        y = x - y;
+        x = x - y;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+
+
+}
