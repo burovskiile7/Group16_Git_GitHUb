06d77084179a982b95bf687cdc60f9c59fd4ed43
Nika <nikapozdniakova@gmail.com>
2024-02-06 21:27:54 -0800

    task 1, week 2

---
+++ src/week2/nika/NikaTask2.java
@@ -0,0 +1,41 @@
+package week2.nika;
+
+public class NikaTask2 {
+    public static void main(String[] args) {
+
+        System.out.println("Divisible by 15:");
+
+        for (int i = 15; i <= 100; i += 15) {
+            if (i % 15 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println();
+        System.out.print("-------------------------------------------------------");
+
+        System.out.println();
+        System.out.println("Divisible by 5:");
+        for (int i = 5; i <= 100; i+=5) {
+            if (i % 15 !=0){
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println();
+        System.out.print("----------------------------------------------------------------");
+
+
+        System.out.println();
+        System.out.println("Divisible by 3:");
+        for (int i = 3; i <= 100; i+=3) {
+            if (i % 15 !=0){
+                System.out.print(i + " ");
+            }
+        }
+
+
+
+        }
+    }
+
