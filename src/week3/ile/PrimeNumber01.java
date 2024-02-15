710c2810404b65db5edd5b7fff4ca230fc43af27
UCLA-Ile <burovskiile7@users.noreply.github.com>
2024-02-15 00:52:27 -0800

    update task1

--- src/week3/ile/PrimeNumber01.java
+++ src/week3/ile/PrimeNumber01.java
@@ -8,7 +8,6 @@ public class PrimeNumber01 {
             for (int i = 2; i * i <= num; i++) {
                 if (num % i == 0) return false;
             }
-
             return true;
         }
         public static void main(String[] args) {
