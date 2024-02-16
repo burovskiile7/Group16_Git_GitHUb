5c2985b4a51ec602db6c3ee9d8a8675e8246c6df
UCLA-Ile <burovskiile7@users.noreply.github.com>
2024-02-16 11:46:37 -0800

    Upload T1,T2,T3 for week 4

---
+++ src/week4_ile/RemoveDuplicates.java
@@ -0,0 +1,25 @@
+package week4_ile;
+
+public class RemoveDuplicates {
+        public static String removeDup(String input) {
+            if (input == null || input.isEmpty()) {
+                return "";
+            }
+
+               StringBuilder result = new StringBuilder();
+                   result.append(input.charAt(0));
+
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) != input.charAt(i - 1)) {
+                 result.append(input.charAt(i));
+                }
+            }
+
+          return result.toString();
+        }
+
+             public static void main(String[] args) {
+                System.out.println(removeDup("AAABBBCCC")); // ABC
+        }
+    }
+
