bb6fec991903496c2059ce28a630bf52d42e51cd
UCLA-Ile <burovskiile7@users.noreply.github.com>
2024-02-21 23:35:45 -0800

    Update T1,T2 for week 5

---
+++ src/week5/Reverse_Ile.java
@@ -0,0 +1,21 @@
+package week5;
+
+public class Reverse_Ile {
+        public static String reverse(String input) {
+            StringBuilder reversed = new StringBuilder();
+
+            // Iterate through the input string from the end to the start  /
+            for (int i = input.length() - 1; i >= 0; i--) {
+                reversed.append(input.charAt(i));
+            }
+
+            return reversed.toString();
+        }
+
+        public static void main(String[] args) {
+            String input = "ABCDE";
+            String reversedString = reverse(input);
+            System.out.println("Reversed string: " + reversedString);  // The Output will be: EDCBA
+        }
+    }
+
