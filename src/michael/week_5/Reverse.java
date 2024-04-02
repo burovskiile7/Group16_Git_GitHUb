f183ac501edab3172308b2dad9424329aaad952b
Ersin KAYHAN <dekked2438@gmail.com>
2024-04-02 20:06:12 +0300

    Week 5 reverse class is updated.

--- src/michael/week_5/Reverse.java
+++ src/michael/week_5/Reverse.java
@@ -3,9 +3,8 @@ package michael.week_5;
 public class Reverse {
     /*
     * Write a return method that can reverse  String
-        Ex: Reverse("ABCD"); ==> DCBA
-        *
-        *
+     *
+     *
 */
 
     public static void main(String[] args) {
@@ -19,7 +18,8 @@ public class Reverse {
     public static String reverse_A_String_1(String str) {
         String reverse="";
         for(int i=str.length()-1; i >= 0; i--)
-            reverse += str.toCharArray()[i];
+           // reverse += str.toCharArray()[i];
+            reverse += str.charAt(i);
 
         return  reverse;
     }
